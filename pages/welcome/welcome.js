d96a11ae55fc2457b0af56221dd22a1201f5938d
Artiikk <harfild1@ukr.net>
2020-05-23 19:11:19 +0200

    added: OTAA support, fixed: inputs format

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -6,14 +6,15 @@ const token = document.querySelector('#token');
 const form = document.querySelector('form');
 const button = document.querySelector('button');
 
+const formatter = (value) => value.replace(/\s|\s+/g, '').toLowerCase()
 const handleSubmit = (e) => {
   e.preventDefault();
 
   const unlock = eui.value && devAddr.value && gatewayId.value && token.value;
   if (unlock) {
-    localStorage.setItem('eui', eui.value);
-    localStorage.setItem('devAddr', devAddr.value);
-    localStorage.setItem('gatewayId', gatewayId.value);
+    localStorage.setItem('eui', formatter(eui.value));
+    localStorage.setItem('devAddr', formatter(devAddr.value));
+    localStorage.setItem('gatewayId', formatter(gatewayId.value));
     localStorage.setItem('token', token.value);
 
     window.location.href = '/pages/detection/detection.html';
